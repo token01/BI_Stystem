3911ea33e7df7d5be994f1b21803758336b70b39
Administrator <admin@example.com>
2016-06-30 18:02:15 +0800

    项目初始化

---
+++ config/config_db.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zhangtao on 2015/4/15.
+ */
+var db = {};
+
+db.url = 'localhost';
+db.user = 'root';
+db.pwd = 'homelink';
+db.name = 'test';
+db.port = 3306;
+
+module.exports = db;
---
+++ redem.md
@@ -0,0 +1,10 @@
+#启动方式
+supervisor ./www/bin
+通过supervisor启动，每次修改文件都会自动启动
+
+或者通过npm start启动
+
+http://localhost:3000/
+
+
+
